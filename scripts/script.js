c3fb8b89034def21e258942be2d601bdd5928fba
new-name <tanu.sharma@softobiz.com>
2021-05-24 09:10:07 +0530

    js commit

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+let userInput=0;
+userInput =(userInput + 10)*3/2-1;
+console.log(userInput);
+//outputResult(currentResult,'');
+//const can not be re defined
+const num = 12;
+const result = 3 + num;
+const num = 15;
+document.write(num);
+alert(result);
